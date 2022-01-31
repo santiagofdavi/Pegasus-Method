adedc97394d1db2a74aaa9f9d585ab8f2381f24b
santiagofdavi <santiagofdavi@ufmg.br>
2022-01-31 13:05:52 -0300

    First commit

---
+++ include/Pegaso.hpp
@@ -0,0 +1,19 @@
+/* PEGASUS METHOD IN C++  
+
+Created using the F.F.CAMPOS' Algotithm in "Algoritmos Numéricos", LTC 3rd
+
+Author: Davi Ferreira Santiago
+    School of Engineering, Universidade Federal de Minas Gerais
+Date: January 31st, 2022 */
+
+#ifndef PEGASUS_H
+#define PEGASUS_H
+
+double avaliacao_funcao(const double &x);
+
+void print(const double &a, const double &b, const double &Fa, const double &Fb, 
+           const double &x, const double &Fx, const double &deltax, const unsigned &k);
+
+void pegaso(double &a, double &b, double &Toler, unsigned &IterMax);
+
+#endif
